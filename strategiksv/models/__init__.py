af85c5b6d5598f610cd49cff404e42e7e20a1c80
leonelgracias <leonelgracias@gmail.com>
2019-01-31 17:49:25 -0600

    correccion al orden de carga

--- strategiksv/models/__init__.py
+++ strategiksv/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from . import account_move
 from . import company_tax
 from . import caja_chica
+from . import account_move
 from . import products
 from . import landed_cost
