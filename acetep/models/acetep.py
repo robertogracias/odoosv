4f8662679c2cd28d4756485f812af7deeda1e2ef
kevintoloza <tolozakevin96@gmail.com>
2019-09-16 13:47:42 -0600

    base funsionando

--- acetep/models/acetep.py
+++ acetep/models/acetep.py
@@ -1,19 +1,20 @@
+# -*- coding: utf-8 -*-
 from odoo import api, models, fields, _
 
 class Nivel(models.Model):
     _name = 'acetep.nivel'
-    nombre = fields.Char(string='Nombre')
+    name = fields.Char(string='Nombre')
 
 class Seccion(models.Model):
     _name ='acetep.seccion' 
-    nombre = fields.Char(string='Nombre')
+    name = fields.Char(string='Nombre')
     horario = fields.Char(string='horario')
     nivel_id = fields.Many2one(comodel_name='acetep.nivel', string='Nivel')
 
 
 class Periodo(models.Model):
     _name = 'acetep.periodo'
-    nombre = fields.Char(string='Nombre')
+    name = fields.Char(string='Nombre')
 
 class Partersv(models.Model):
     _inherit = 'res.partner'
@@ -21,5 +22,5 @@ class Partersv(models.Model):
 
 class Ninio(models.Model):
     _name = 'acetep.nino'
-    nombre = fields.Char(string='Nombre')
+    name = fields.Char(string='Nombre')
     partner_id = fields.Many2one(comodel_name='res.partner' ,string='Contacto')
