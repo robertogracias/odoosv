25b2ec8091f37f67e0a6d0896ed3ead89a8a32d7
kevintoloza <tolozakevin96@gmail.com>
2019-09-20 18:27:52 -0600

    Nuevos campos al modelos y vistas, relacione falta afinar detalles y llenar la base

--- acetep/models/acetep.py
+++ acetep/models/acetep.py
@@ -8,7 +8,7 @@ class Nivel(models.Model):
 class Seccion(models.Model):
     _name ='acetep.seccion' 
     name = fields.Char(string='Nombre')
-    horario = fields.Char(string='horario')
+    horario = fields.Char(string='Horario')
     nivel_id = fields.Many2one(comodel_name='acetep.nivel', string='Nivel')
 
 
@@ -16,14 +16,18 @@ class Periodo(models.Model):
     _name = 'acetep.periodo'
     name = fields.Char(string='Nombre')
 
-class Partersv(models.Model):
+class Partersv(models.Model): #cliente padre
     _inherit = 'res.partner'
     nino_id = fields.One2many(comodel_name='acetep.nino', inverse_name='partner_id' )
+    how = fields.Selection([('Redes sociales','Redes sociales'), ('Referido','Referido'), ('Familiar','Familiar'), ('Pagina web','Pagina web'),  ('Pasaba por el local','Pasaba por el local'), ('Alianza','Alianza'), ('Otros','Otros')])
 
-class Ninio(models.Model):
+class Nino(models.Model):
     _name = 'acetep.nino'
     name = fields.Char(string='Nombre')
-    partner_id = fields.Many2one(comodel_name='res.partner' ,string='Contacto')
+    birthday = fields.Date(string="Fecha de cumpleaños")
+    partner_id = fields.Many2one(comodel_name='res.partner' ,string='Padre') #Nombre del padre
+    employed_id = fields.Many2one(comodel_name='hr.employee' ,string='Instructora de la clase de prueba')
+    datetest = fields.Date(string="Fecha de la clase de prueba")
 
 class Invoice(models.Model):
     _inherit='account.invoice.line'
@@ -32,3 +36,7 @@ class Invoice(models.Model):
     seccion_id =fields.Many2one(comodel_name='acetep.seccion', string='seccion')
     periodo_id =fields.Many2one(comodel_name='acetep.periodo', string='periodo')
 
+class Employed(models.Model):
+    _inherit='hr.employee'
+    nino_id = fields.One2many(comodel_name='acetep.nino', inverse_name='employed_id' )    
+      
